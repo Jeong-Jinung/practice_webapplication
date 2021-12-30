47efe286da1ce64ab00a9b6597d3739da11309df
JeongJinUng <jinung.dev@gmail.com>
2021-12-30 23:57:39 +0900

    Initial commit

---
+++ src/main/java/com/study/account/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.study.account;
+
+import com.study.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true) // write lock을 안써서 조금이라도 성능의 이점을 가져온다.
+public interface AccountRepository extends JpaRepository<Account, Long> {
+
+
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
