f9cf64f1de8fe7bdaa3b9b0a01d94b29c2cbf0f4
JeongJinUng <jinung.dev@gmail.com>
2022-01-10 23:38:17 +0900

    관심주제 자동완성

--- src/main/java/com/study/settings/SettingsController.java
+++ src/main/java/com/study/settings/SettingsController.java
@@ -1,5 +1,7 @@
 package com.study.settings;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.account.AccountRepository;
 import com.study.account.AccountService;
 import com.study.domain.Account;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -44,6 +47,7 @@ public class SettingsController {
     private final NicknameValidator nicknameValidator;
     private final AccountRepository accountRepository;
     private final TagRepository tagRepository;
+    private final ObjectMapper objectMapper;
 
     @InitBinder("passwordForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -123,10 +127,15 @@ public class SettingsController {
     }
 
     @GetMapping(SETTINGS_TAGS_URL)
-    public String updateTag(@CurrentUser Account account, Model model) {
+    public String updateTag(@CurrentUser Account account, Model model) throws JsonProcessingException {
         model.addAttribute(account);
+
         Set<Tag> tags = accountService.getTags(account);
         model.addAttribute("tags", tags.stream().map(Tag::getTitle).collect(Collectors.toList()));
+
+        List<String> allTags = tagRepository.findAll().stream().map(Tag::getTitle).collect(Collectors.toList());
+        model.addAttribute("whiteList", objectMapper.writeValueAsString(allTags));
+
         return SETTINGS_TAGS_VIEW_NAME;
     }
 
